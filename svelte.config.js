43819b6ec760e5bb54d0f3828f7a71ace617c2cd
Mark Manders <mmjmanders@gmail.com>
2026-02-10 11:44:28 +0100

    Add dynamic base path configuration in SvelteKit based on environment

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 
+const { argv, env } = process;
+
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
+const config = {
+	kit: {
+		adapter: adapter(),
+		paths: {
+			base: argv.includes('dev') ? '' : env.BASE_PATH
+		}
+	}
+};
 
 export default config;
