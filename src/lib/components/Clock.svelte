fc94807dc358105e74cd65e9f74fc8927e1c2a0d
Mark Manders <mmjmanders@gmail.com>
2026-02-11 12:31:46 +0100

    Feat/dynamic load numbers (#4)
    * Refactor Clock component: replace static SVG imports with dynamic `getNixieSvg` helper function for digit rendering.
    
    * Replace `getNixieSvg` helper with dynamic raw SVG imports for Nixie digit rendering in the Clock component.
    
    * Simplify Clock component by inlining dynamic raw SVG imports for Nixie digit rendering.
    
    * Refactor Clock component: use `padStart` for consistent zero-padding of hours and minutes.

--- src/lib/components/Clock.svelte
+++ src/lib/components/Clock.svelte
@@ -1,27 +1,33 @@
 <script lang="ts">
-	import nixie0 from '$lib/assets/Nixie 0.svg';
-	import nixie1 from '$lib/assets/Nixie 1.svg';
-	import nixie2 from '$lib/assets/Nixie 2.svg';
-	import nixie3 from '$lib/assets/Nixie 3.svg';
-	import nixie4 from '$lib/assets/Nixie 4.svg';
-	import nixie5 from '$lib/assets/Nixie 5.svg';
-	import nixie6 from '$lib/assets/Nixie 6.svg';
-	import nixie7 from '$lib/assets/Nixie 7.svg';
-	import nixie8 from '$lib/assets/Nixie 8.svg';
-	import nixie9 from '$lib/assets/Nixie 9.svg';
-
-	const nixies = [nixie0, nixie1, nixie2, nixie3, nixie4, nixie5, nixie6, nixie7, nixie8, nixie9];
-
 	const { time }: { time: Date } = $props();
 
-	const [hours, minutes] = $derived(time.toLocaleTimeString().split(':'));
-	const [hourLeft, hourRight] = $derived(hours.split('').map(Number));
-	const [minuteLeft, minuteRight] = $derived(minutes.split('').map(Number));
+	const [hourLeft, hourRight] = $derived(
+		String(time.getHours()).padStart(2, '0').split('').map(Number)
+	);
+	const [minuteLeft, minuteRight] = $derived(
+		String(time.getMinutes()).padStart(2, '0').split('').map(Number)
+	);
 </script>
 
 <div class="clock">
-	<div class="digit"><img alt={hourLeft.toString()} src={nixies[hourLeft]} /></div>
-	<div class="digit"><img alt={hourRight.toString()} src={nixies[hourRight]} /></div>
-	<div class="digit"><img alt={minuteLeft.toString()} src={nixies[minuteLeft]} /></div>
-	<div class="digit"><img alt={minuteRight.toString()} src={nixies[minuteRight]} /></div>
+	<div class="digit">
+		{#await import(`../assets/Nixie ${hourLeft}.svg?raw`) then { default: svg }}
+			{@html svg}
+		{/await}
+	</div>
+	<div class="digit">
+		{#await import(`../assets/Nixie ${hourRight}.svg?raw`) then { default: svg }}
+			{@html svg}
+		{/await}
+	</div>
+	<div class="digit">
+		{#await import(`../assets/Nixie ${minuteLeft}.svg?raw`) then { default: svg }}
+			{@html svg}
+		{/await}
+	</div>
+	<div class="digit">
+		{#await import(`../assets/Nixie ${minuteRight}.svg?raw`) then { default: svg }}
+			{@html svg}
+		{/await}
+	</div>
 </div>
